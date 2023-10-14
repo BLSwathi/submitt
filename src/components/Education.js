b9ca47f53b4d0c2fe25ba5561e9d79de89eacc40
Swathi01~ <belide1409@gmail.com>
2023-10-14 20:51:56 +0530

    first commit

---
+++ src/components/Education.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Education = () => {
+  return (
+    <section>
+                <h2>Education</h2>
+            
+                <h3>Vijayavani Public School, Chitto'or</h3>
+                <p>10th standard</p>
+                <p>Graduation Year: 2019</p>
+            
+            
+                <h3>Sri Vivekananda Junior College, Chittoor</h3>
+                <p>Intermediate</p>
+                <p>Graduation Year: 2021</p>
+            
+            
+                <h3>Vellore Institute of Technology, Vellore</h3>
+                <p>B.tech (Computer Science Engineering with specialization in Bioinformation)</p>
+                <p>Graduation Year: 2025</p>
+            
+            
+    </section>
+  );
+}
+
+export default Education;
